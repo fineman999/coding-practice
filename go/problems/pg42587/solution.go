f290cc383e1d16bfd66020c779cc6b4f35ea850b
chan <33cks1423@naver.com>
2026-03-16 12:15:20 +0900

    solve: [PG][42587] 프로세스[go]

--- go/problems/pg42587/solution.go
+++ go/problems/pg42587/solution.go
@@ -3,10 +3,187 @@
 // 난이도: lv2
 // 태그: stack,queue
 //
-// 시간복잡도: O(?)
-// 공간복잡도: O(?)
+// 시간복잡도: O(n)
+// 공간복잡도: O(n)
 package pg42587
 
-func Solution() int {
-	panic("not implemented")
+import (
+	"container/list"
+	"sort"
+)
+
+func Solution(priorities []int, location int) int {
+	return solution04(priorities, location)
+}
+
+func solution04(priorities []int, location int) int {
+	type Data struct {
+		p, idx int
+	}
+	n := len(priorities)
+	// 딱 필요한 만큼만 공간 할당
+	size := n + 1
+	q := make([]Data, size)
+	head, tail := 0, 0
+
+	pop := func() Data {
+		d := q[head]
+		head = (head + 1) % size
+		return d
+	}
+
+	push := func(d Data) {
+		q[tail] = d
+		tail = (tail + 1) % size
+	}
+
+	for i, p := range priorities {
+		push(Data{p, i})
+	}
+
+	importSort := priorities
+	sort.Slice(importSort, func(i, j int) bool {
+		return importSort[i] > importSort[j]
+	})
+
+	count, pIdx := 0, 0
+	for head != tail {
+		curr := pop()
+
+		if curr.p < importSort[pIdx] {
+			push(curr)
+		} else {
+			count++
+			if curr.idx == location {
+				return count
+			}
+			pIdx++
+		}
+	}
+	return count
+}
+
+func solution03(priorities []int, location int) int {
+	type Data struct {
+		p, idx int
+	}
+	q := list.New()
+	for i, p := range priorities {
+		q.PushBack(Data{p, i})
+	}
+	sort.Sort(sort.Reverse(sort.IntSlice(priorities)))
+
+	count, pIdx := 0, 0
+	for q.Len() > 0 {
+		front := q.Front()
+		if front == nil {
+			return count
+		}
+		curr := front.Value.(Data)
+		q.Remove(front)
+
+		if curr.p < priorities[pIdx] {
+			q.PushBack(curr)
+		} else {
+			count++
+			if curr.idx == location {
+				return count
+			}
+			pIdx++
+		}
+	}
+	return count
+}
+
+func solution02(priorities []int, location int) int {
+	type Data struct {
+		p, idx int
+	}
+
+	q := make([]Data, 0, len(priorities))
+	for i, p := range priorities {
+		q = append(q, Data{p, i})
+	}
+	head := 0 // 맨 앞을 가리키는 포인터
+	count := 0
+	pIdx := 0
+	sort.Sort(sort.Reverse(sort.IntSlice(priorities)))
+
+	for head < len(q) {
+		curr := q[head]
+		head++
+
+		if curr.p < priorities[pIdx] {
+			q = append(q, curr)
+		} else {
+			count++
+			if curr.idx == location {
+				return count
+			}
+			pIdx++
+		}
+	}
+	return count
+}
+
+func solution01(priorities []int, location int) int {
+	q := NewQueue[Data]()
+	for i, priority := range priorities {
+		q.Push(Data{
+			index:   i,
+			element: priority,
+		})
+	}
+	sort.Slice(priorities, func(i, j int) bool {
+		return priorities[i] > priorities[j]
+	})
+	count := 0
+	for _, priority := range priorities {
+		for !q.IsEmpty() {
+			popLeft := q.PopLeft()
+			if popLeft.element < priority {
+				q.Push(popLeft)
+				continue
+			}
+			count++
+			if popLeft.index == location {
+				return count
+			}
+			break
+		}
+	}
+	return count
+}
+
+type Queue[T any] interface {
+	PopLeft() T
+	Push(T)
+	IsEmpty() bool
+}
+
+type queue[T any] struct {
+	data []T
+}
+
+func (q *queue[T]) PopLeft() T {
+	front := q.data[0]
+	q.data = q.data[1:]
+	return front
+}
+
+func (q *queue[T]) Push(t T) {
+	q.data = append(q.data, t)
+}
+
+func (q *queue[T]) IsEmpty() bool {
+	return len(q.data) == 0
+}
+
+type Data struct {
+	element int
+	index   int
+}
+
+func NewQueue[T any]() Queue[T] {
+	return &queue[T]{}
 }
--- go/problems/pg42587/solution_test.go
+++ go/problems/pg42587/solution_test.go
@@ -1,31 +1,54 @@
 package pg42587
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 func TestSolution(t *testing.T) {
 	tests := []struct {
-		name string
-		want int
+		name       string
+		priorities []int
+		location   int
+		want       int
 	}{
 		{
-			name: "기본 케이스",
-			want: 0,
+			name:       "예제1",
+			priorities: []int{2, 1, 3, 2},
+			location:   2,
+			want:       1,
 		},
 		{
-			name: "엣지 케이스",
-			want: 0,
+			name:       "예제2",
+			priorities: []int{1, 1, 9, 1, 1, 1},
+			location:   0,
+			want:       5,
 		},
 		{
-			name: "큰 입력",
-			want: 0,
+			name:       "모두 같은 우선순위",
+			priorities: []int{1, 1, 1, 1},
+			location:   2,
+			want:       3,
+		},
+		{
+			name:       "내 위치가 가장 앞이고 가장 큰 경우",
+			priorities: []int{9, 1, 1, 1},
+			location:   0,
+			want:       1,
+		},
+		{
+			name:       "내 위치가 가장 뒤이고 한 번에 실행됨",
+			priorities: []int{1, 1, 1, 9},
+			location:   3,
+			want:       1,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := Solution()
+			got := Solution(tt.priorities, tt.location)
 			if got != tt.want {
-				t.Errorf("Solution() = %v, want %v", got, tt.want)
+				t.Errorf("Solution(%v, %d) = %d, want %d", tt.priorities, tt.location, got, tt.want)
 			}
 		})
 	}
@@ -33,7 +56,17 @@ func TestSolution(t *testing.T) {
 
 // 가장 연산량이 많은 최악의 케이스(큰 입력)"
 func BenchmarkSolution(b *testing.B) {
+	n := 100
+	priorities := make([]int, n)
+	for i := range priorities {
+		priorities[i] = rand.Intn(9) + 1 // 1~9 랜덤 우선순위
+	}
+	location := rand.Intn(n)
+
+	// 타이머 리셋
+	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
-		Solution()
+		Solution(priorities, location)
 	}
 }
