e971dc4d44223e70f35a7bd69cdfc29627015069
chan <33cks1423@naver.com>
2026-03-16 12:35:27 +0900

    solve: [PG][42587] 프로세스(python)

--- python/problems/pg42587/solution.py
+++ python/problems/pg42587/solution.py
@@ -4,10 +4,22 @@ https://programmers.co.kr/learn/courses/30/lessons/42587
 난이도: lv2
 태그: stack,queue
 
-시간복잡도: O(?)
-공간복잡도: O(?)
+시간복잡도: O(n^2)
+공간복잡도: O(n)
 """
+from collections import deque
 
+def solution(priorities: list[int], location: int):
+    queue = deque(enumerate(priorities))
+    priorities.sort(reverse=True)
+    count = 0
+    while queue:
+        idx, p = queue.popleft()
 
-def solution() -> int:
-    raise NotImplementedError("풀이를 작성하세요")
+        if p < priorities[count]:
+            queue.append((idx, p))
+        else:
+            count+=1
+            if idx == location:
+                return count
+    return count
--- python/problems/pg42587/test_solution.py
+++ python/problems/pg42587/test_solution.py
@@ -3,16 +3,20 @@ from .solution import solution
 
 
 class TestSolution:
-    """[PG] 42587 - 프로세스"""
 
-    def test_basic(self):
-        """기본 케이스"""
-        assert solution() == 0
+    @pytest.mark.parametrize(
+        "priorities, location, expected",
+        [
+            ([2, 1, 3, 2], 2, 1),           # 문제 예시 1
+            ([1, 1, 9, 1, 1, 1], 0, 5),     # 문제 예시 2
+        ]
+    )
+    def test_solution(self, priorities, location, expected):
+        assert solution(priorities, location) == expected
 
-    def test_edge(self):
-        """엣지 케이스"""
-        pass
-
-    def test_large_input(self):
-        """큰 입력"""
-        pass
+    def test_large_case(self, benchmark):
+        # 큰 입력: 1~9 우선순위가 반복되고, location은 마지막
+        priorities = [i % 9 + 1 for i in range(100)]
+        location = 99
+        result = benchmark(solution ,priorities, location)
+        assert 1 <= result <= 100   # 정답 범위에는 들어가는지 체크 (구체적 정답은 로직에 따라 다름)
